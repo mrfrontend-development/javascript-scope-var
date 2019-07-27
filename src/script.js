df0d57c2b7c78af9d34a984773c788dfaaf6b59c
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2019-07-27 21:10:11 +0200

    Uncommend the appartmentComplex function

--- src/script.js
+++ src/script.js
@@ -6,38 +6,38 @@ var worldPre = document.querySelector('[data-code="world"]')
 var worldScope = { scope: 'World' }
 
 /** APPARTMENT COMPLEX */
-// function appartmentComplex() {
-// 	// Declare variables for scopes
-// 	var appartmentsComplexPre = document.querySelector('[data-code="appartments-complex"]')
-// 	var appartmentsComplexScope = { scope: 'Appartments complex' }
+function appartmentComplex() {
+	// Declare variables for scopes
+	var appartmentsComplexPre = document.querySelector('[data-code="appartments-complex"]')
+	var appartmentsComplexScope = { scope: 'Appartments complex' }
 
-// 	/** APPARTMENT nr 15 */
-// 	function appartment() {
-// 		// Declare variables for scopes
-// 		var appartmentPre = document.querySelector('[data-code="appartment"]')
-// 		var appartmentScope = { scope: 'Appartment nr 15' }
+	/** APPARTMENT nr 15 */
+	function appartment() {
+		// Declare variables for scopes
+		var appartmentPre = document.querySelector('[data-code="appartment"]')
+		var appartmentScope = { scope: 'Appartment nr 15' }
 
-// 		/** Living room */
-// 		function livingRoom() {
-// 			// Declare variables for scopes
-// 			var livingRoomPre = document.querySelector('[data-code="living-room"]')
-// 			var livingRoomScope = { scope: 'Living room' }
+		/** Living room */
+		function livingRoom() {
+			// Declare variables for scopes
+			var livingRoomPre = document.querySelector('[data-code="living-room"]')
+			var livingRoomScope = { scope: 'Living room' }
 
-// 			// 	Set code into right scope
-// 			setCodeInPreTag(livingRoomPre, livingRoomScope)
-// 		}
-// 		livingRoom()
+			// 	Set code into right scope
+			setCodeInPreTag(livingRoomPre, livingRoomScope)
+		}
+		livingRoom()
 
-// 		// 	Set code into right scope
-// 		setCodeInPreTag(appartmentPre, appartmentScope)
-// 	}
-// 	appartment()
+		// 	Set code into right scope
+		setCodeInPreTag(appartmentPre, appartmentScope)
+	}
+	appartment()
 
-// 	// 	Set code into right scope
-// 	setCodeInPreTag(appartmentsComplexPre, appartmentsComplexScope)
-// }
+	// 	Set code into right scope
+	setCodeInPreTag(appartmentsComplexPre, appartmentsComplexScope)
+}
 
-// appartmentComplex()
+appartmentComplex()
 
 // 	Set code into right scope
 setCodeInPreTag(worldPre, worldScope)
